05677f55c278b4701046fd519cbc2eae9c422e30
Avinash Sarguru <avinashsarguru@gmail.com>
2020-01-22 10:17:18 -0600

    added random route and controller for route

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,11 @@
 class PlayersController < ApplicationController
+    
+    def index    
+        render :json => Player.all.as_json(only: [:name, :position, :team])
+    end
+
+    def random
+        render :json => Player.all.first.as_json
+    end
+
 end
